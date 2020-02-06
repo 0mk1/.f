317c1d92794f904f85590a9daf0842e77a3c92e3
Mateusz Kamycki <m.kamycki@merixstudio.com>
2020-02-06 13:44:54 +0100

    dotfiles sync 20200206

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-echo -n "Setup Mac Preferences"
-echo -n "Switch Capslock to Esc"
-echo -n "Install firefox, iterm, slack, zoom, chrome, spectacle"
-echo -n "Setup Firefox settings and Vim, Https, ublock, auto delete cookie"
+echo -n "Install firefox, iterm, slack, zoom, chrome, spotify, spectacle"
 echo -n "Install brew"
 echo -n "Installing from brew"
-# brew install openssh pass zsh tmux neovim kube-ps1 fd fzf git gnupg kubectx pinentry reattach-to-user-namespace the_silver_searcher wget curl
+# brew install ssh pass zsh tmux neovim kube-ps1 fd fzf git gnupg kubectx pinentry reattach-to-user-namespace the_silver_searcher wget curl minikube
 echo -n "Install antigen"
-echo -n "Install gcloud"
 echo "Remember to use chsh to change to /bin/zsh"
 # chsh -s /bin/zsh
 echo -n "Import/Create your gpg key and ssh keys"
